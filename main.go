5ff63279f7fd75b92d665d81eefba06f58acd9b6
Niels Koster <n.koster@portavita.eu>
2021-11-12 23:59:22 +0100

    Topics route separated into file

--- main.go
+++ main.go
@@ -49,28 +49,7 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/topics", func(c *fiber.Ctx) error {
-		rows, err := db.Query("SELECT * FROM dist_kafka_topic")
-		if err != nil {
-			return c.Status(500).SendString(err.Error())
-		}
-		defer rows.Close()
-		result := Topics{}
-		id := ""
-		// this is basically a dummy because we don't need the id,
-		// but we need to pass something to rows.Scan(), like &id
-		for rows.Next() {
-			topic := Topic{}
-			if err := rows.Scan(&id, &topic.Kafka_topic); err != nil {
-				return err
-			}
-			fmt.Println(id, topic.Kafka_topic)
-			result.Topics = append(result.Topics, topic)
-		}
-		fmt.Printf("%d topic%s received.", len(result.Topics),
-			(map[bool]string{true: "", false: "s"})[len(result.Topics) == 1])
-		return c.JSON(result)
-	})
+	app.Get("/topics", topics)
 
 	log.Fatal(app.Listen(":3000"))
 
---
+++ topics.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func topics(c *fiber.Ctx) error {
+	rows, err := db.Query("SELECT * FROM dist_kafka_topic")
+	if err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+	defer rows.Close()
+	result := Topics{}
+	id := ""
+	// this is basically a dummy because we don't need the id,
+	// but we need to pass something to rows.Scan(), like &id
+	for rows.Next() {
+		topic := Topic{}
+		if err := rows.Scan(&id, &topic.Kafka_topic); err != nil {
+			return err
+		}
+		fmt.Println(id, topic.Kafka_topic)
+		result.Topics = append(result.Topics, topic)
+	}
+	fmt.Printf("%d topic%s received.", len(result.Topics),
+		(map[bool]string{true: "", false: "s"})[len(result.Topics) == 1])
+	return c.JSON(result)
+}
