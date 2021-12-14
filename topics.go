fad85f5a40fe8602c4333bb3c2df51a088ebf7a8
Niels Koster <n.koster@portavita.eu>
2021-12-14 23:32:23 +0100

    Update pg query

--- topics.go
+++ topics.go
@@ -7,21 +7,19 @@ import (
 )
 
 func topics(c *fiber.Ctx) error {
-	rows, err := db.Query("SELECT * FROM dist_kafka_topic")
+	rows, err := db.Query("SELECT * FROM kafka_topic")
 	if err != nil {
+		fmt.Println(err)
 		return c.Status(500).SendString(err.Error())
 	}
 	defer rows.Close()
 	result := Topics{}
 	id := ""
-	// this is basically a dummy because we don't need the id,
-	// but we need to pass something to rows.Scan(), like &id
 	for rows.Next() {
 		topic := Topic{}
 		if err := rows.Scan(&id, &topic.Kafka_topic); err != nil {
 			return err
 		}
-		fmt.Println(id, topic.Kafka_topic)
 		result.Topics = append(result.Topics, topic)
 	}
 	fmt.Printf("%d topic%s received.", len(result.Topics),
