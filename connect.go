8c14ec9eb1a96d3947d0107472f25cc61ddbd08b
Niels Koster <n.koster@portavita.eu>
2021-12-15 00:47:55 +0100

    Update env name PG_ROOTCERT_PATH

--- connect.go
+++ connect.go
@@ -15,18 +15,22 @@ func Connect() error {
 	port, _ := strconv.Atoi(os.Getenv("PG_PORT"))
 	sslcert := os.Getenv("PG_CERT_PATH")
 	sslkey := os.Getenv("PG_KEY_PATH")
-	sslrootcert := os.Getenv("PG_CA_PATH")
+	sslrootcert := os.Getenv("PG_ROOTCERT_PATH")
 	sslmode := os.Getenv("PG_SSL_MODE")
 	user := os.Getenv("PG_USER")
 	database := os.Getenv("PG_DATABASE")
 
 	db, err = sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s sslmode=%s sslcert=%s sslkey=%s sslrootcert=%s database=%s",
 		host, port, user, sslmode, sslcert, sslkey, sslrootcert, database))
+
 	if err != nil {
 		return err
 	}
+
 	if err = db.Ping(); err != nil {
 		return err
 	}
+
 	return nil
+
 }
