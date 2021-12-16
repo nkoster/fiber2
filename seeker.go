fcce058cd83e31c4b7433f91f5f0cdd8a7295f23
Niels Koster <n.koster@portavita.eu>
2021-12-16 18:31:01 +0100

    Add query canceling logic

--- seeker.go
+++ seeker.go
@@ -7,21 +7,57 @@ import (
 )
 
 func seeker(c *fiber.Ctx) error {
+
+	var err error
+
 	payload := struct {
 		QueryKafkaTopic      string `json:"queryKafkaTopic"`
 		QueryIdentifierType  string `json:"queryIdentifierType"`
 		QueryIdentifierValue string `json:"queryIdentifierValue"`
+		QueryId              string `json:"queryId"`
 	}{}
 	if err := c.BodyParser(&payload); err != nil {
 		return err
 	}
-	rows, err := db.Query(`SELECT /*greetz_from_golang*/
-		* from func_identifier(in_identifier_value => $3,
-		in_identifier_type => $2,
-		in_kafka_topic => $1,
-		in_kafka_offset => null,
-		in_kafka_partition => null)
-	`, payload.QueryKafkaTopic, payload.QueryIdentifierType, payload.QueryIdentifierValue)
+
+	sqlKill := `WITH pids AS (
+		/*notthisone*/
+		SELECT pid
+		FROM   pg_stat_activity
+		WHERE  query LIKE '%%/*%s*/%%'
+		AND    query NOT LIKE '%%/*notthisone*/%%'
+		AND    state='active'
+	)
+	SELECT pg_cancel_backend(pid) FROM pids
+	`
+	sqlKill = fmt.Sprintf(sqlKill, payload.QueryId)
+
+	killed, err := db.Query(sqlKill)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer killed.Close()
+
+	counter := 0
+	for killed.Next() {
+		counter++
+	}
+
+	if counter > 0 {
+		fmt.Println("Previous query was cancelled.")
+	}
+
+	sqlSelect := `SELECT /*%s*/
+	* from func_identifier(in_identifier_value => $3,
+	in_identifier_type => $2,
+	in_kafka_topic => $1,
+	in_kafka_offset => null,
+	in_kafka_partition => null)
+	`
+	sqlSelect = fmt.Sprintf(sqlSelect, payload.QueryId)
+
+	rows, err := db.Query(sqlSelect, payload.QueryKafkaTopic, payload.QueryIdentifierType, payload.QueryIdentifierValue)
 	if err != nil {
 		fmt.Println(err)
 		return c.Status(500).SendString(err.Error())
@@ -41,7 +77,7 @@ func seeker(c *fiber.Ctx) error {
 		}
 		result.Messages = append(result.Messages, message)
 	}
-	fmt.Printf("%d message%s received.", len(result.Messages),
+	fmt.Printf("%d message%s received.\n", len(result.Messages),
 		(map[bool]string{true: "", false: "s"})[len(result.Messages) == 1],
 	)
 	return c.JSON(result)
