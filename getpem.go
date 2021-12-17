4563bf823e1b28dd84e81437c445984f3d087162
Niels Koster <niels.koster@portavita.nl>
2021-12-17 20:25:19 +0100

    Load pem data

--- getpem.go
+++ getpem.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
 func getPem() error {
+
+	var err error
+
+	resp, err := http.Get("https://oic.dev.portavita.nl/certs")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sb := string(body)
+	log.Println(sb)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	return nil
+
 }
