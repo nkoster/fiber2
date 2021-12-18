2b5dbe6d7cc7ca0d8c9192c340498ddd8ab01aee
Niels Koster <n.koster@portavita.eu>
2021-12-18 17:42:08 +0100

    Add timer

--- topics.go
+++ topics.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func topics(c *fiber.Ctx) error {
+
+	defer timeTrack(time.Now(), "topics")
+
 	rows, err := db.Query("SELECT * FROM kafka_topic")
 	if err != nil {
 		fmt.Println(err)
 		return c.Status(500).SendString(err.Error())
 	}
+
 	defer rows.Close()
+
 	result := Topics{}
+
 	id := ""
 	for rows.Next() {
 		topic := Topic{}
@@ -22,8 +29,11 @@ func topics(c *fiber.Ctx) error {
 		}
 		result.Topics = append(result.Topics, topic)
 	}
-	fmt.Printf("pg: %d topic%s received.\n", len(result.Topics),
+
+	fmt.Printf("topics: %d topic%s received.\n", len(result.Topics),
 		(map[bool]string{true: "", false: "s"})[len(result.Topics) == 1],
 	)
+
 	return c.JSON(result)
+
 }
