6009e39f16543d163cb4ed7cc26410841b23daa0
Niels Koster <niels.koster@portavita.nl>
2021-12-20 17:08:55 +0100

    Add introspect - not okay yet, WIP

--- oidc.go
+++ oidc.go
@@ -10,7 +10,20 @@ func oidc(c *fiber.Ctx) error {
 
 	fmt.Println("oidc:", c.Get("Authorization"))
 
-	c.Next()
+	access_token := getAccessToken(c.Get("Authorization"))
+
+	if len(access_token) == 0 {
+		fmt.Println("No token supplied.")
+		// no token supplied logic, WIP
+	}
+
+	fmt.Println(access_token)
+	// check if access_token is valid via /introspect
+	if err := validateAccessToken(access_token); err != nil {
+		fmt.Println("Invalid token")
+	} else {
+		c.Next()
+	}
 
 	return nil
 
