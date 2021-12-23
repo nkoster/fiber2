bad94ab328aa8e4ab78c8e96876cb3b9c375a9e8
Niels Koster <niels.koster@portavita.nl>
2021-12-23 22:59:18 +0100

    Be more verbose when things fail

--- oidc.go
+++ oidc.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -12,17 +13,21 @@ func oidc(c *fiber.Ctx) error {
 
 	if len(access_token) == 0 {
 		// no token supplied
+		fmt.Println("No token supplied")
 		return c.Status(401).SendString("Please login first.")
 	}
 
 	// check if access_token is valid via /introspect
 	if body := validateAccessToken(access_token); body == "" {
+		fmt.Println("Access token introspect failed")
 		return c.Status(401).SendString("Please login first.")
 	} else {
 		// Parse json in body
 		// Test if token is active
+		fmt.Println(body)
 		var token_state TokenState
 		json.Unmarshal([]byte(body), &token_state)
+		// fmt.Println(token_state)
 		if token_state.Active {
 			// get SSO context
 			ssoContext := getSsoContext(access_token)
@@ -33,11 +38,13 @@ func oidc(c *fiber.Ctx) error {
 					// To do: verify scope
 					return c.Next()
 				} else {
+					fmt.Println("SSO context failed")
 					return c.Status(401).SendString("Please login first.")
 				}
 			}
 			c.Next()
 		} else {
+			fmt.Println("Token was not active")
 			return c.Status(401).SendString("Please login first.")
 		}
 	}
