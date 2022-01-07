4fee13f49009332e5e7ffe1cf4f4cd8f608a094e
Niels Koster <niels.koster@portavita.nl>
2022-01-07 15:33:12 +0100

    Load .env before test

--- connect_test.go
+++ connect_test.go
@@ -1,9 +1,18 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/joho/godotenv"
+)
 
 func TestConnect(t *testing.T) {
+
+	if err := godotenv.Load(); err != nil {
+		t.Error("No .env file in current directory.")
+	}
+
 	if Connect() != nil {
-		t.Error("DB connection failed")
+		t.Error("DB connection failed.")
 	}
 }
