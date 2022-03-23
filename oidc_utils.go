a05bc199be849ab6113318103ab71b8850dd8c66
Niels Koster <niels.koster@portavita.nl>
2022-03-23 16:28:15 +0100

    Remove ssoContext stuff, check scope instead

--- oidc_utils.go
+++ oidc_utils.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,8 +9,7 @@ import (
 	"net/url"
 	"os"
 	"strings"
-
-	jwt "github.com/dgrijalva/jwt-go"
+	// jwt "github.com/dgrijalva/jwt-go"
 )
 
 func getAccessToken(s string) string {
@@ -66,70 +65,10 @@ func validateAccessToken(token string) string {
 
 }
 
-func getSsoContext(token string) string {
-
-	client := &http.Client{}
-
-	URL := os.Getenv("OIDC_SSO_CONTEXT")
-
-	req, err := http.NewRequest("GET", URL, nil)
-
-	req.Header.Add("Authorization", "Bearer "+token)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	resp, err := client.Do(req)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	bodyText, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return string(bodyText)
-
-}
-
 type Organization struct {
 	PvEntityId int `json:"pv_entity_id"`
 }
 
 type Sso struct {
 	Organization Organization
-}
-
-func verifySsoContext(token string, key string) bool {
-
-	parts := strings.Split(token, ".")
-
-	signingKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(key))
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	err = jwt.SigningMethodRS256.Verify(strings.Join(parts[0:2], "."), parts[2], signingKey)
-
-	if err != nil {
-		fmt.Println(err)
-		return false
-	}
-
-	ssoJson, _ := jwt.DecodeSegment(parts[1])
-
-	var sso Sso
-
-	json.Unmarshal(ssoJson, &sso)
-
-	fmt.Println(string(ssoJson))
-
-	// test if pv_entity_id == 3 == pvt_support1 / pv_entity_id == niels == 17899744)
-	return sso.Organization.PvEntityId == 17899744
-
 }
