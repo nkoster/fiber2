08b83e38f088998033f575b964fcec4fbe54f690
Niels <niels.koster@portavita.nl>
2022-04-05 15:37:52 +0200

    Scope from .env + Better logging and error handling

--- oidc.go
+++ oidc.go
@@ -3,40 +3,43 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func oidc(c *fiber.Ctx) error {
 
-	access_token := getAccessToken(c.Get("Authorization"))
+	accessToken := getAccessToken(c.Get("Authorization"))
 
-	if len(access_token) == 0 {
-		// no token supplied
+	if len(accessToken) == 0 {
 		fmt.Println("oidc: No token supplied.")
 		return c.Status(401).SendString("Please login first.")
 	}
 
-	// check if access_token is valid via /introspect
-	if body := validateAccessToken(access_token); body == "" {
+	// check if accessToken is valid via /introspect.
+	if body := validateAccessToken(accessToken); body == "" {
 		fmt.Println("oidc: Access token introspect failed.")
 		return c.Status(401).SendString("Please login first.")
 	} else {
-		// Parse json in body
-		// Test if token is active
-		var token_state TokenState
-		json.Unmarshal([]byte(body), &token_state)
-		// Verify if token is active and if the scope is correct
-		if token_state.Scope == "openid kafkasearch" {
-			fmt.Println("oidc: Allowed:", token_state.Scope, access_token)
-			if token_state.Active {
+		// Test if token is active.
+		var tokenState TokenState
+		if err := json.Unmarshal([]byte(body), &tokenState); err != nil {
+			fmt.Println("oidc: Cannot unmarshal JSON.")
+			return c.Status(500).SendString("Cannot unmarshal JSON.")
+		}
+		// Verify if scope is correct, and if the token is active.
+		if strings.Contains(tokenState.Scope, os.Getenv("OIDC_SCOPE")) {
+			if tokenState.Active {
+				fmt.Printf("oidc: Allow %s, %s\n", accessToken, tokenState.Scope)
 				return c.Next()
 			} else {
 				fmt.Println("oidc: Token was not active.")
 				return c.Status(401).SendString("Please login first.")
 			}
 		} else {
-			fmt.Printf("oidc: Scope %s not allowed\n", token_state.Scope)
+			fmt.Printf("oidc: Scope %s not allowed\n", tokenState.Scope)
 			return c.Status(401).SendString("Please login first.")
 		}
 	}
